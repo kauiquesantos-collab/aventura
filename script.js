c3edb5273b83c10f2108b1d5f17eb2e9da6471b8
kauiquesantos-collab <kauique.santos@escola.pr.gov.br>
2025-09-04 10:59:06 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const avanca= document. querySelectorAll("btn.proximo");
+const reimiciarbtn=document.getElementById('btn-reiniciar');
+
+avanca.forEach(button =>{
+  button.addconst avanca= document. querySelectorAll("btn.proximo");
+  Eventlistener ('click',function() }
+   const at     console.error('elementocom iD "${proximopasso}"nao encontrado.'
+atual.classList.remover('ativo');
+ const proximolemento= document.     console.error('elementocom iD "${proximopasso}"nao encontrado.'     console.error('elementocom iD "${proximopasso}"nao encontrado.'     console.error('elementocom iD "${proximopasso}"nao encontrado.'entById(proximopasso);
+ console.error('elementocom iD "${prox      console.error('elementocom iD "${proximopasso}"nao encontrado.'    console.error('elementocom iD "${proximopasso}"nao encontrado.'imopasso}"nao encontrado.'
+ 
+ if (proximoelementos.classlist.add('ativo');     console.error('elementocom iD "${proximopasso}"nao encontrado.'
+ {els {      console.error('elementocom iD "${proximopasso}"nao encontrado.'
+
+   {
+  {);
+{);
